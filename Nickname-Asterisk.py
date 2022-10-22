f57590ea83746b255e1b3f64aaf3441132ceff47
MariaBernadete Baroto <bernadetebaroto@gmail.com>
2022-10-22 13:25:01 +0800

    letter-D

--- Nickname-Asterisk.py
+++ Nickname-Asterisk.py
@@ -2,16 +2,22 @@
 
 name="ADETH"
 
-letter_A = [[" " for i in range(5)] for j in range(5)]
-letter_D = [[" " for i in range(4)] for j in range(5)]
-letter_E = [[" " for i in range(4)] for j in range(5)]
-letter_T = [[" " for i in range(5)] for j in range(5)]
-letter_H = [[" " for i in range(5)] for j in range(5)]
+letter_A = [[" " for i in range(7)] for j in range(5)]
+letter_D = [[" " for i in range(7)] for j in range(5)]
+letter_E = [[" " for i in range(7)] for j in range(5)]
+letter_T = [[" " for i in range(7)] for j in range(5)]
+letter_H = [[" " for i in range(7)] for j in range(5)]
 
 #Code for letter A
 
-for row in range(5):
-    for col in range(9):
+for row in range(7):
+    for col in range(5):
         if ((col==0 or col==4) and row!=0) or ((row==0 or row==2) and col>0 and col<4):
             letter_A[row][col]="#"
-        
+
+#Code for letter D
+
+for row in range(7):
+    for col in range(5):
+        if (col==0) or (col==4 and (row!=0 and row!=6)) or ((row==0 or row==6) and (col>0 and col<4)):
+            letter_D[row][col]="#"      
