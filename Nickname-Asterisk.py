fedbda54c1f0246833f16bd7fcb89a0c067d790a
MariaBernadete Baroto <bernadetebaroto@gmail.com>
2022-10-22 12:06:36 +0800

    Creating Nickname using asterisk

---
+++ Nickname-Asterisk.py
@@ -0,0 +1,15 @@
+#Program that will print my nickname into asterisk character only
+
+name="ADETH"
+
+letter_A = [[" " for i in range(5)] for j in range(5)]
+letter_D = [[" " for i in range(4)] for j in range(5)]
+letter_E = [[" " for i in range(4)] for j in range(5)]
+letter_T = [[" " for i in range(5)] for j in range(5)]
+letter_H = [[" " for i in range(5)] for j in range(5)]
+
+#Code for letter A
+
+for row in range(5):
+    for col in range(9):
+        if 
