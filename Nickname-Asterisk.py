6f64223e269f116f524fac20979d5abf2d64c390
MariaBernadete Baroto <bernadetebaroto@gmail.com>
2022-10-22 13:29:49 +0800

    letter-E

--- Nickname-Asterisk.py
+++ Nickname-Asterisk.py
@@ -21,3 +21,13 @@ for row in range(7):
     for col in range(5):
         if (col==0) or (col==4 and (row!=0 and row!=6)) or ((row==0 or row==6) and (col>0 and col<4)):
             letter_D[row][col]="#"      
+
+
+
+#Code for letter E
+
+for row in range(7):
+    for col in range(5):
+        if col==0 or ((row==0 or row==3 or row==6) and (col>0)):
+            letter_E[row][col]="#"  
+
