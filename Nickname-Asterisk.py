727ea7f8c71a33b6d5469cae3910e531f846ee87
MariaBernadete Baroto <bernadetebaroto@gmail.com>
2022-10-22 13:34:24 +0800

    letter-T

--- Nickname-Asterisk.py
+++ Nickname-Asterisk.py
@@ -31,3 +31,11 @@ for row in range(7):
         if col==0 or ((row==0 or row==3 or row==6) and (col>0)):
             letter_E[row][col]="#"  
 
+#Code for letter T
+
+for row in range(7):
+    for col in range(5):
+        if col==2 or (row==0 and col!=2):
+            letter_T[row][col]="#" 
+
+
